08c48a68841677394bdbf43599f8a45a8c3d0f51
AmaliaAfidatulAini <ainiamalia242@gmail.com>
2026-02-20 08:29:06 +0700

    percobaan1

---
+++ mahasiswa03.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class mahasiswa03 {
+
+        String nama;
+        String nim;
+        String kelas;
+        double ipk;
+
+        void tampilkanInformasi() {
+            System.out.println("Nama: " + nama);
+            System.out.println("NIM: " + nim);
+            System.out.println("IPK: " + ipk);
+            System.out.println("Kelas: " + kelas);
+        }
+
+        void ubahKelas(String kelasBaru) {
+            kelas = kelasBaru;
+        }
+
+        void updateIpk(double ipkBaru) {
+            ipk = ipkBaru;
+        }
+
+        String nilaiKinerja() {
+            if (ipk >= 3.5) {
+            return "Kinerja sangat baik";
+        } else if (ipk >= 2.0) {
+            return "Kinerja cukup";
+        } else {
+            return "Kinerja kurang";
+        }
+    }
+}
